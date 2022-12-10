9b3b1a8e72c3f85492fe5b37b2b43c907f8eb70b
Mauriciocunha1996 <mauricio.nascimento.cunha@gmail.com>
2022-12-09 23:12:09 -0300

    getHELP

---
+++ GetHelp/src/main/Aula.java
@@ -0,0 +1,50 @@
+package main;
+
+public class Aula {
+    private String nome;
+    private String disciplina;
+    private String nivelDeEnsino;
+    int idAula;
+    public Aula(){
+        super();
+    }
+    public Aula(String nome, int idAula, String disciplina, String nivelDeEnsino) {
+        this.idAula =  idAula;
+        this.nome = nome;
+        this.disciplina = disciplina;
+        this.nivelDeEnsino = nivelDeEnsino;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdAula() {
+        return idAula;
+    }
+    public String getDisciplina() {
+        return disciplina;
+    }
+    public String getNivelDeEnsino() {
+        return nivelDeEnsino;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void seIdAula(int idAula) {
+        this.idAula = idAula;
+    }
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+    public void setNivelDeEnsino(String nivelDeEnsino){
+        this.nivelDeEnsino = nivelDeEnsino;
+    }
+    public String print() {
+		return "Nome:" + nome + " ID:" + idAula + " Disciplina:" + disciplina + " Nivel de Ensino:" + nivelDeEnsino;
+	}
+
+
+}
